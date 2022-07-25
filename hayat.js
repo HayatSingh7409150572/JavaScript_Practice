185ac77ad99d14ad7d43a5736410f82b227751f2
Hayat60403 <hayat.singh@ola.com>
2022-07-25 12:16:44 +0530

    function to convert a first letter of a string into  a upperCase

---
+++ hayat.js
@@ -0,0 +1,28 @@
+
+// let hayat=(a)=>{
+//     let temp =a.split('')
+//     let newTemp = temp.toUpperCase()
+//     return newTemp
+
+
+// }
+
+
+// console.log(hayat("mynameishayat"))
+
+
+//----------To capitalize the first letter of the string-----------By using charAt() and Slice-----//
+
+// const capitalize = (s) => {
+//     return s.charAt(0).toUpperCase() + s.slice(1)
+//   }
+  
+//   capitalize('codax');
+
+//--------By using regex ----------//
+
+// const capitalize = (s) =>{
+//   return s.replace(/^\w/, (c) => c.toUpperCase());
+// }
+
+// console.log(capitalize('codax'));
